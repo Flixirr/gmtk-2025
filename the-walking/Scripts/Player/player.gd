6d9567667da8c757d306b6ddc3fe79079f520795
Filip Krężel <45013654+Flixirr@users.noreply.github.com>
2025-07-30 21:35:43 +0200

    basic character movement

---
+++ the-walking/Scripts/Player/player.gd
@@ -0,0 +1,55 @@
+extends CharacterBody3D
+class_name Player
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 3.0
+
+@onready var cam_rotate = $CamRotate
+@onready var head = $CamRotate/Head
+@onready var camera = $CamRotate/Head/Camera3D
+@onready var raycast = $CamRotate/Head/RayCast3D
+
+@export var max_cam_angle = 60
+@export var min_cam_angle = -60
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		cam_rotate.rotate_y(-event.relative.x * GlobalVariables.mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * GlobalVariables.mouse_sensitivity)
+		raycast.rotate_x(-event.relative.y * GlobalVariables.mouse_sensitivity)
+		
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(min_cam_angle), deg_to_rad(max_cam_angle))
+		raycast.rotation.x = clamp(camera.rotation.x, deg_to_rad(min_cam_angle), deg_to_rad(max_cam_angle))
+
+# head bob
+var t_bob = 0.0
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = JUMP_VELOCITY
+	
+	var input_dir := Input.get_vector("WalkLeft", "WalkRight", "WalkFwd", "WalkBack")
+	var direction = (cam_rotate.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	velocity.x = (direction.x * SPEED)
+	velocity.z = (direction.z * SPEED)
+
+	
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	raycast.transform.origin = camera.transform.origin
+
+func _headbob(time):
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
